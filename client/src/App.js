90dc5e0534421434ff57dce3c42a755c13672763
haseeb21111 <20055911@mydbs.ie>
2025-07-22 23:46:28 +0500

    protected routes implementation

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
 import Inbox from './pages/Inbox';
-
+import ProtectedRoute from './pages/ProtectedRoute';
 
 function App() {
+  const isLoggedIn = !!localStorage.getItem('userEmail');
+
   return (
     <Router>
       <div style={{ padding: '10px' }}>
@@ -14,15 +16,33 @@ function App() {
         <nav>
           <Link to="/register" style={{ marginRight: '10px' }}>Register</Link>
           <Link to="/login" style={{ marginRight: '10px' }}>Login</Link>
-          <Link to="/send-message" style={{ marginRight: '10px' }}>Send Message</Link>
-          <Link to="/inbox" style={{ marginRight: '10px' }}>Inbox</Link>
+          {isLoggedIn && (
+            <>
+              <Link to="/send-message" style={{ marginRight: '10px' }}>Send Message</Link>
+              <Link to="/inbox" style={{ marginRight: '10px' }}>Inbox</Link>
+            </>
+          )}
         </nav>
         <hr />
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/send-message" element={<Chat />} />
-          <Route path="/inbox" element={<Inbox />} />
+          <Route
+            path="/send-message"
+            element={
+              <ProtectedRoute>
+                <Chat />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/inbox"
+            element={
+              <ProtectedRoute>
+                <Inbox />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
